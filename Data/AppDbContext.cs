355be865bb975e32cde451cc87aac90694db1c53
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2024-10-08 23:24:44 +0600

    migration appied to the database for role seeding

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -13,6 +13,24 @@ namespace Identity.Management.Api.Data
 
     // Add the models to the context here
 
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+      SeedRoles(builder);
+
+    }
+
+    private static void SeedRoles(ModelBuilder builder)
+    {
+      // Seed roles here
+      builder.Entity<IdentityRole>().HasData(
+          new IdentityRole { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
+          new IdentityRole { Id = "2", Name = "User", NormalizedName = "USER" },
+          new IdentityRole { Id = "3", Name = "Customer", NormalizedName = "CUSTOMER" }
+      );
+    }
+
   }
 }
 
